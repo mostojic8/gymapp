77ce7e5848ef58e7638bc758747892e5e5298e21
Marko Ostojić <m.ostojiic8@gmail.com>
2026-07-24 16:34:58 +0200

    Fix iOS camera settings in index.html

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "508c2481167063d84d272ddb76b84852
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2b6f577e7537f8d50a7d9c9a8a3b9404",
+"flutter_bootstrap.js": "863275f99f9a98ae02b26396691ee5e4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
